d7a829e8fc623947b66200ee5362ed5caede9b64
Abhay Singh Rawat <129993139+Abhaysinghrawat@users.noreply.github.com>
2024-02-21 14:34:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const form = document.getElementById('registrationForm');
+form.addEventListener('submit', async function (event) {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const userData = {};
+    formData.forEach((value, key) => {
+        userData[key] = value;
+    });
+    try {
+        const response = await fetch('https://node-js-task-1.onrender.com/api/user', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userData)
+        });
+        const responseData = await response;
+        if(responseData.status === 201)
+        console.log('User registered successfully:', responseData);
+    } catch (error) {
+        console.error('Error registering user:', error.message);
+    }
+});
+
+
